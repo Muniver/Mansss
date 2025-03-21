68128b823d27c95bd5fd5142f19f21d6b388264c
Muniver <ya62398@gmail.com>
2025-03-21 06:46:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
 
     // التحقق من بيانات الاعتماد
     if (username === "30404261200811" && password === "Vxdt@7429") {
-        // إخفاء صفحة الدخول وعرض صفحة النتائج
+        // إخفاء صفحة الدخول وعرض صفحة الأيقونات
         document.getElementById("loginPage").style.display = "none";
-        document.getElementById("resultPage").style.display = "block";
+        document.getElementById("iconsPage").style.display = "grid";
     } else {
         alert("اسم المستخدم أو كلمة المرور غير صحيحة.");
     }
 });
+
+// دالة لعرض صفحة النتائج
+function showResultPage() {
+    document.getElementById("iconsPage").style.display = "none";
+    document.getElementById("resultPage").style.display = "block";
+}
 
 // دالة لعرض صفحة استعادة كلمة المرور
 function forgotPassword() {
